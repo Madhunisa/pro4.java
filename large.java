4c169795078d67e8f22db44365c43564af55afe7
Madhunisa <nisasenthil97@gmail.com>
2017-06-08 11:58:05 +0530

    Create large.java

---
+++ large.java
@@ -0,0 +1,25 @@
+# pro4.java
+import java.io.*;
+import java.util.*;
+public class large3no
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int i=s.nextInt();
+        int j=s.nextInt();
+        int k=s.nextInt();
+        if(i>=k && i>=j)
+        {
+            System.out.println(i);
+        }
+        if(j>=i && j>=k)
+        {
+            System.out.println(j);
+        }
+        if(k>=i && k>=j)
+        {
+            System.out.println(k);
+        }
+    }
+}
